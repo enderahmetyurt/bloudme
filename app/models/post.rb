563491502a86911e061ef3d2660b509d695e4f14
Ender AhmeT Yurt <enderyurt@gmail.com>
2024-12-20 14:43:03 +0300

    Add rich text

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   scope :published, -> { where(published: true) }
   scope :recent, -> { order(created_at: :desc) }
+
+  has_rich_text :content
 end
