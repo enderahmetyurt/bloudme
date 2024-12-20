02010e8fcea2591cb2f6c2ffd96578b101a791f0
Ender AhmeT Yurt <enderyurt@gmail.com>
2024-12-20 14:05:42 +0300

    Small additions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  scope :published, -> { where(published: true) }
+  scope :recent, -> { order(created_at: :desc) }
 end
