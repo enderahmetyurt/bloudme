74c037365e84c19fa6e5d39b80a6f91c60fd3457
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-01-04 12:48:44 +0300

    Add user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "home/index"
   resources :posts
   resource :session
   resources :passwords, param: :token
   resource :registration, only: %i[new create]
+  resources :dashboard
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
