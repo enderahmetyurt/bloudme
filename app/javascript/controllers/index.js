0b3adc58839854b525999354a1f7095436e5f1b6
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-01-16 17:47:55 +0300

    Add chacter counter

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import CharacterCounter from '@stimulus-components/character-counter'
+
 eagerLoadControllersFrom("controllers", application)
+application.register("character-counter", CharacterCounter)
