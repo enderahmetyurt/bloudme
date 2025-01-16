de27094126f5052933b5d587a30e11cc2f7bc2bc
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-01-16 16:50:51 +0300

    Fix small UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,14 @@ end
 100.times do
   sentence = Faker::Lorem.sentence.truncate(rand(10..99), separator: ' ')
   paragraph = Faker::Lorem.paragraph.truncate(rand(50..399), separator: ' ')
+  date = Faker::Date.between(from: 1.month.ago, to: Date.today)
 
   Post.create(
     title: sentence,
     content: paragraph,
     user_id: User.all.sample.id,
-    published: [ true, false ].sample
+    published: [ true, false ].sample,
+    created_at: date,
+    updated_at: date,
   )
 end
