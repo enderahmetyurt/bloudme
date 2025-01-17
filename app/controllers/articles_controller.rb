2c7dbe984c389922df90038e684663550989239f
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-01-17 13:46:36 +0300

    Add recent scope to Article model; update ArticlesController to fetch recent articles and change fallback URL in authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles =  Article.joins(feed: :user).where(feeds: { user: Current.user })
+    @articles =  Article.joins(feed: :user).where(feeds: { user: Current.user }).recent
     @feed = Feed.new
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :feed
+  scope :recent, -> { order(published_at: :desc) }
 end
