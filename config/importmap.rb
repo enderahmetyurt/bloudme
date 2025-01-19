cab6ac03445cb97c1643fe694f57c4aab507d964
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-01-19 22:19:44 +0300

    Remove character counter functionality from post form and related imports

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "@stimulus-components/character-counter", to: "@stimulus-components--character-counter.js" # @5.0.0
