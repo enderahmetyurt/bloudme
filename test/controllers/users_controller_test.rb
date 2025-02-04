0fdb8d9ffb36d081c2b91966eb5d37508c84e961
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-04 14:28:39 +0300

    Add user tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:one)
+    @feeds = feeds(:one)
   end
 
   def sign_in_user
@@ -16,5 +17,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_session_path
     assert_nil assigns(:user)
     assert_nil assigns(:posts)
+  end
+
+  test "should show authenticated user and their feeds" do
+    sign_in_user
+
+    get user_url(@user)
+
+    assert_response :success
+
+    assert assigns(:user)
+    assert assigns(:feeds)
   end
 end
