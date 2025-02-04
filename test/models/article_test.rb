584af6aafcbedcd7ad8efd13a36eb2705348bb69
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-04 14:39:17 +0300

    Add user and article model tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @feed = feeds(:one)
+    @article = articles(:one)
+    @unread_article = articles(:unread)
+  end
+
+  test "recent scope should order by published_at desc" do
+    assert_equal Article.order(published_at: :desc), Article.recent
+  end
+
+  test "unread scope should return only unread articles" do
+    assert_includes Article.unread, @unread_article
+    assert_not_includes Article.unread, @article
+  end
+
+  test "by_current_user scope should return articles belonging to the user's feeds" do
+    assert_includes Article.by_current_user(@user), @article
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test "email should be present" do
+    @user.email_address = " "
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email_address = @user.email_address.upcase
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should be normalized" do
+    @user.email_address = "  TEST@Example.com "
+    @user.save
+    assert_equal "test@example.com", @user.reload.email_address
+  end
+
+  test "password should be secure" do
+    assert @user.authenticate("password")
+    assert_not @user.authenticate("wrongpassword")
+  end
+
+  test "avatar_url should be set on create" do
+    user = User.create(email_address: "newuser@example.com", password: "password")
+    assert user.avatar_url.present?
+    assert_match(/#{User::DICE_BEAR_URL}/, user.avatar_url)
+  end
+
+  test "nick_name should be set on create" do
+    user = User.create(email_address: "newuser@example.com", password: "password")
+    assert_equal "newuser", user.nick_name
+  end
 end
