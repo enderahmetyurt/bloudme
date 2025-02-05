f8fcabfda18eb9d1c514cf2f4052e27f42608ff9
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-05 14:37:36 +0300

    Restrict user profile access and add alert message for unauthorized views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    if @user != Current.user
+      redirect_to root_path, alert: t("users.show.alert")
+    end
+
     @feeds = @user.feeds.recent
   end
 end
