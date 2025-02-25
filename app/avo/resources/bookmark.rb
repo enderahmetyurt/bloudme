fdbc1c5c8756ae3ab9c22547123f91289b5e5af1
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-25 17:17:28 +0300

    Refactor Bookmark resource by removing commented-out search code and unnecessary whitespace

--- app/avo/resources/bookmark.rb
+++ app/avo/resources/bookmark.rb
@@ -4,12 +4,10 @@ class Avo::Resources::Bookmark < Avo::BaseResource
   # self.search = {
   #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
   # }
-  
+
   def fields
     field :id, as: :id
     field :article, as: :belongs_to
     field :user, as: :belongs_to
   end
 end
-
-
