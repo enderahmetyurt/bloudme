b6f0dcd9e303b714779d9d0fb2b46a427c00d41f
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-02-28 17:38:35 +0300

    Fix tests

--- test/services/rss_parser_service_test.rb
+++ test/services/rss_parser_service_test.rb
@@ -2,17 +2,16 @@ require "test_helper"
 class RssParserServiceTest < ActiveSupport::TestCase
   test "fetches and parses an RSS feed" do
     VCR.use_cassette("feed") do
-      response = RssParserService.fetch_and_parse("https://www.ruby-lang.org/en/feeds/news.rss")
+      response = RssParserService.fetch_and_parse("https://www.ruby-lang.org")
 
-      assert_equal "Ruby News", response[:title]
+      assert_equal "Ruby RSS News", response[:title]
       assert_equal 10, response[:entries].size
-      assert_equal "Ruby 3.4.2 Released", response[:entries].first[:title]
-      assert_equal "https://www.ruby-lang.org/en/news/2025/02/14/ruby-3-4-2-released/", response[:entries].first[:link]
+      assert_equal "Ruby 3.4.2 veröffentlicht", response[:entries].first[:title]
+      assert_equal "https://www.ruby-lang.org/de/news/2025/02/14/ruby-3-4-2-released/", response[:entries].first[:link]
     end
   end
 
   test "fetches and parses a YouTube channel" do
-    skip("Not fetching YouTube channel properly")
     VCR.use_cassette("youtube") do
       response = RssParserService.fetch_and_parse("https://www.youtube.com/@eayurt")
 
