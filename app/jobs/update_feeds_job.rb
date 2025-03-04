410796df79eca3c07f483e7f18adfd8868933512
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-03-04 13:59:38 +0300

    Fix rename attribute

--- app/jobs/update_feeds_job.rb
+++ app/jobs/update_feeds_job.rb
@@ -3,7 +3,7 @@ class UpdateFeedsJob < ApplicationJob
 
   def perform(*args)
     Feed.find_each do |feed|
-      parsed_feed = RssParserService.fetch_and_parse(feed.url)
+      parsed_feed = RssParserService.fetch_and_parse(feed.feed_url)
       next unless parsed_feed
 
       parsed_feed[:entries].each do |entry|
