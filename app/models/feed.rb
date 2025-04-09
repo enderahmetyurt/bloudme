592ed8f8ed80de7937c4b254d5ca8700fce85245
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-04-09 18:13:39 +0300

    Add uniqueness validation for feed_url scoped to user_id

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,7 +2,7 @@ class Feed < ApplicationRecord
   belongs_to :user
   has_many :articles, dependent: :destroy
 
-  validates :feed_url, presence: true
+  validates :feed_url, presence: true, uniqueness: { scope: :user_id }
 
   scope :recent, -> { order(created_at: :desc) }
 end
