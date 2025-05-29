1b8a21228ca4cf52f8375d7e4fc142f05f3ab6a0
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-05-29 17:03:14 +0300

    Update email confirmation mailer and template for improved URL handling

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@bloud.me"
+
   layout "mailer"
+
+  def default_url_options
+    Rails.application.config.action_mailer.default_url_options
+  end
 end
