ef3d04ebc12ccd3a740ee9c7174eb1aa91f2892e
Ender AhmeT Yurt <enderyurt@gmail.com>
2025-07-14 07:58:00 +0300

    Add tests for locale handling and session state in HomeController

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -22,4 +22,49 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
     assert_template layout: "home"
   end
+
+  test "should handle locale parameter when not authenticated" do
+    get root_url, params: { locale: "tr" }
+
+    assert_response :success
+    assert_template :index
+    assert_template layout: "home"
+    assert_equal "tr", session[:locale]
+  end
+
+  test "should handle locale parameter when authenticated" do
+    user = users(:one)
+    sign_in_as(user)
+
+    get root_url, params: { locale: "se" }
+
+    assert_redirected_to articles_path
+    assert_equal "se", session[:locale]
+  end
+
+  test "should allow unauthenticated access" do
+    get root_url
+
+    assert_response :success
+    assert_no_match /redirect/i, response.body
+  end
+
+  test "should handle multiple consecutive requests correctly" do
+    3.times do
+      get root_url
+      assert_response :success
+      assert_template :index
+      assert_template layout: "home"
+    end
+  end
+
+  test "should maintain session state across multiple requests" do
+    user = users(:one)
+    sign_in_as(user)
+
+    3.times do
+      get root_url
+      assert_redirected_to articles_path
+    end
+  end
 end
