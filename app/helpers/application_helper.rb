c36e5a56ef7c178878f62ace93933fe2cc3d9b80
Ender Ahmet Yurt <enderyurt@gmail.com>
2025-10-17 21:50:15 +0300

    Fix security problem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
   def spinner
     '<svg width="20" height="20" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_P7sC"/></svg>'
   end
+
+  def sanitize_href(url)
+    return "" if url.blank?
+
+    begin
+      uri = URI.parse(url)
+      return "" unless ["http", "https", "mailto"].include?(uri.scheme)
+      url
+    rescue URI::InvalidURIError
+      ""
+    end
+  end
 end
